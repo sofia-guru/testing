ec198c023aa2bff09d04233114f7b9897985775b
sofia-guru <159521770+sofia-guru@users.noreply.github.com>
2024-02-17 18:27:15 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,33 @@
+from openai import OpenAI
 import streamlit as st
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key=st.secrets["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"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
