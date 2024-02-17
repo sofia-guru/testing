259f46ec33e22b003770b1a03322d8c79f196334
sofia-guru <159521770+sofia-guru@users.noreply.github.com>
2024-02-17 18:54:17 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,34 +4,5 @@ import streamlit as st
 
 st.title("ChatGPT-like clone")
 
-os.environ['API_KEY']='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'
-openai.api_key = os.getenv('API_KEY')
-print(openai.api_key)
-model_name="gpt-3.5-turbo"
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
